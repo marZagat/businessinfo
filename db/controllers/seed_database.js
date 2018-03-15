f86485908e3a4a8454523b6785e8ba8751672502
David Seidenberg <davidseidenberg@gmail.com>
2018-03-15 11:33:12 -0700

    Fix open mongoose connection import, successfully close DB after seeding

--- db/controllers/seed_database.js
+++ db/controllers/seed_database.js
@@ -1,44 +1,41 @@
 const makeFakeRestaurant = require('../../fakeDataGenerator.js');
-const database = require('../models/restaurant.js');
-const insert = database.insert;
+// const database = require('../models/restaurant.js');
+// const insert = database.insert;
 const MongoClient = require('mongodb').MongoClient;
 
 const url = 'mongodb://localhost:27017';
 
 MongoClient.connect(url, (err, client) => {
-  const restaurants = client.db('businessinfo').collection('restaurants');
+    const restaurants = client.db('businessinfo').collection('restaurants');
 
-  async function seedDatabase() {
-    // await mongoose.connection.dropDatabase();
-    await client.db('businessinfo').dropDatabase();
-    console.log('database dropped');
-  
-  
-    let id = 0;
-    let fakeRestaurants;
-  
-    const generateFakeRestaurantData = (numRecords) => {
-      for (let i = 0; i < numRecords; i++) {
-        fakeRestaurants.push(makeFakeRestaurant(id));
-        id++;
+    async function seedDatabase() {
+      await client.db('businessinfo').dropDatabase();
+      console.log('database dropped');
+    
+    
+      let id = 0;
+      let fakeRestaurants;
+    
+      const generateFakeRestaurantData = (numRecords) => {
+        for (let i = 0; i < numRecords; i++) {
+          fakeRestaurants.push(makeFakeRestaurant(id));
+          id++;
+        }
+        console.log('inserted id ', id);
       }
-      console.log('inserted id ', id);
-    }
-  
-    for (var i = 0; i < 10; i++) {
-      fakeRestaurants = [];
-      generateFakeRestaurantData(10000);
-  
-      await restaurants.insertMany(fakeRestaurants);
-    }
-  
-    console.log('database seeded');
-    // client.close();
-    console.log('database disconnected');
-  };
+    
+      for (var i = 0; i < 5; i++) {
+        fakeRestaurants = [];
+        generateFakeRestaurantData(10000);
+    
+        await restaurants.insertMany(fakeRestaurants);
+      }
+    
+      console.log('database seeded');
+      // client.close();
+    };
 
-
-  seedDatabase().then(client.close()).catch(error => console.error(error.stack));
+  seedDatabase().then(async () => {await client.close()}).catch(error => console.error(error.stack));
 });
 
 

35dc8726271dfc618d72a265400f9e4a67573385
David Seidenberg <davidseidenberg@gmail.com>
2018-03-19 10:52:09 -0700

    Refactored restaurant and hours table batch seed functions into single seedDb function

--- psdb/seedPSDB.js
+++ psdb/seedPSDB.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const host = process.env.POSTGRES_HOST || 'localhost';
 const username = process.env.POSTGRES_USER || 'davidseid';
 const password = process.env.POSTGRES_PW || 'marzagat';
@@ -14,40 +15,39 @@ const db = pgp(cn);
 const connectDB = async () => {
   await db.connect();
   console.log('dbConnected');
-}
-
-const seedDb = async () => {
-  await connectDB();
-  
-}
+};
 
 const insertRestaurantBatch = async () => {
   const restaurantColumns = ['place_id', 'formatted_address', 'international_phone_number', 'url', 'lat', 'lng', 'weekday_text'];
-  const csRestaurants = new pgp.helpers.ColumnSet(restaurantColumns, {table: 'restaurants'});
-  const fakeRestaurants = [{place_id: '6', formatted_address: '765 Price Canyon Rd', international_phone_number: '805 345 9258', url: 'david.com', lat: 23.4, lng: 45.7, weekday_text: 'sample'}];
+  const csRestaurants = new pgp.helpers.ColumnSet(restaurantColumns, { table: 'restaurants' });
+  const fakeRestaurants = [{
+    place_id: '6', formatted_address: '765 Price Canyon Rd', international_phone_number: '805 345 9258', url: 'david.com', lat: 23.4, lng: 45.7, weekday_text: 'sample',
+  }];
   const insertionQueryRestaurants = pgp.helpers.insert(fakeRestaurants, csRestaurants);
 
-  db.none(insertionQueryRestaurants)
-    .then(data => {
-      console.log('success');
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}
+  await db.none(insertionQueryRestaurants);
+  console.log('inserted restaurants');
+};
 
+const insertHoursBatch = async () => {
+  const hoursColumns = ['restaurant_id', 'weekday', 'open_time', 'close_time'];
+  const csHours = new pgp.helpers.ColumnSet(hoursColumns, { table: 'hours' });
+  const fakeHours = [{
+    restaurant_id: '6', weekday: 0, open_time: '800', close_time: '1900',
+  }];
+  const insertionQueryHours = pgp.helpers.insert(fakeHours, csHours);
 
-const hoursColumns = ['restaurant_id', 'weekday', 'open_time', 'close_time'];
-const csHours = new pgp.helpers.ColumnSet(hoursColumns, {table: 'hours'});
-const fakeHours = [{restaurant_id: '6', weekday: 0, open_time: '800', close_time: '1900'}];
-const insertionQueryHours = pgp.helpers.insert(fakeHours, csHours);
+  await db.none(insertionQueryHours);
+  console.log('inserted hours');
+};
 
-db.none(insertionQueryHours)
-  .then(data => {
-    console.log('success');
-  })
-  .catch(error => {
-    console.error(error);
-  })
+const seedDb = async () => {
+  await connectDB();
+  await insertRestaurantBatch();
+  await insertHoursBatch();
+  console.log('postgres database seeded');
+};
+
+seedDb();
 
 module.exports = db;

805173fe8e41570309a0257cbdfcd6942c5b9dec
David Seidenberg <davidseidenberg@gmail.com>
2018-03-21 20:08:01 -0700

    Split database script into two separate database creation files, one for each database

---
+++ psdb/createRelationalDB.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const pg = require('pg');
+const host = process.env.POSTGRES_HOST || 'localhost';
+const username = process.env.POSTGRES_USER || 'davidseid';
+const password = process.env.POSTGRES_PW || 'marzagat';
+const port = parseInt(process.env.POSTGRES_PORT, 10) || 5432;
+
+const client = new pg.Client(`postgres://${username}:${password}@${host}:${port}/businessinfoflat`);
+client.connect();
+const query = client.query(`CREATE TABLE restaurants(
+                            place_id TEXT, 
+                            formatted_address VARCHAR(120), 
+                            international_phone_number VARCHAR(40), 
+                            url VARCHAR(40), 
+                            lat REAL, 
+                            lng REAL, 
+                            weekday_text VARCHAR(250))`)
+  .then(() => client.query(`CREATE TABLE hours(
+                            restaurant_id TEXT, 
+                            weekday INTEGER, 
+                            open_time TEXT, 
+                            close_time TEXT)`))
+  .then(() => client.query('CREATE INDEX restaurant_id ON hours (restaurant_id)'))
+  .then(() => client.end());
--- psdb/createSingleTableDB.js
+++ psdb/createSingleTableDB.js
@@ -5,8 +5,9 @@ const username = process.env.POSTGRES_USER || 'davidseid';
 const password = process.env.POSTGRES_PW || 'marzagat';
 const port = parseInt(process.env.POSTGRES_PORT, 10) || 5432;
 
-const client = new pg.Client(`postgres://${username}:${password}@${host}:${port}/businessinfo`);
+const client = new pg.Client(`postgres://${username}:${password}@${host}:${port}/businessinfoflat`);
 client.connect();
+<<<<<<< 41ddc9618f5ee84a8501a87031a49491eb60d275
 const query = client.query(`CREATE TABLE restaurants(
                             place_id TEXT, 
                             formatted_address VARCHAR(120), 
@@ -21,4 +22,7 @@ const query = client.query(`CREATE TABLE restaurants(
                             open_time TEXT, 
                             close_time TEXT)`))
   .then(() => client.query('CREATE INDEX restaurant_id ON hours (restaurant_id)'))
+=======
+const query = client.query('CREATE TABLE restaurants(place_id TEXT, formatted_address VARCHAR(120), international_phone_number VARCHAR(40), url VARCHAR(40), lat REAL, lng REAL, weekday_text VARCHAR(250), monday_open TEXT, monday_close TEXT, tuesday_open TEXT, tuesday_close TEXT, wednesday_open TEXT, wednesday_close TEXT, thursday_open TEXT, thursday_close TEXT, friday_open TEXT, friday_close TEXT, saturday_open TEXT, saturday_close TEXT, sunday_open TEXT, sunday_close TEXT)')
+>>>>>>> Refactor database setup script to use only one table
   .then(() => client.end());

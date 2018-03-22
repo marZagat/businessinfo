ed118b5a299f3f3a62dbd687fa96b3839f75b8fe
David Seidenberg <davidseidenberg@gmail.com>
2018-03-22 11:33:05 -0700

    Finish refactoring and testing mongo generation and seed scripts

--- mongodb/data/nestedSchema/generateNestedData.js
+++ mongodb/data/nestedSchema/generateNestedData.js
@@ -1,19 +1,16 @@
 const faker = require('faker');
 
-// *** Fake Hours Data Generator to make restaurant hours more realistic *** //
-const makeFakeHoursData = () => {
-  const fakeHoursData = {
-    periods: [],
-    weekdayText: [],
-  };
+// *** Fake Hours Data Generator to generate restaurant hours more realistic *** //
+const generateHoursData = () => {
+  const periods = [];
+
   const openTimes = ['06', '07', '08', '09', '10', '11'];
   const closeTimes = ['18', '19', '20', '21', '22', '23'];
-  const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
   for (let i = 0; i < 7; i += 1) {
     const closeTime = closeTimes[Math.floor(Math.random() * 5)];
     const openTime = openTimes[Math.floor(Math.random() * 5)];
-    const fakeDailyHours = {
+    const dailyHours = {
       close: {
         day: i,
         time: `${closeTime}00`,
@@ -23,36 +20,62 @@ const makeFakeHoursData = () => {
         time: `${openTime}00`,
       },
     };
-    fakeHoursData.periods.push(fakeDailyHours);
-
-    const convertHoursToStandard = (militaryHour) => {
-      let hour = parseInt(militaryHour, 10);
-      if (hour > 12) {
-        hour -= 12;
-      }
-      return hour.toString();
-    };
-    fakeHoursData.weekdayText.push(`${weekdays[i]}: ${convertHoursToStandard(openTime)}:00 AM - ${convertHoursToStandard(closeTime)}:00 PM`);
+    periods.push(dailyHours);
   }
 
-  return fakeHoursData;
+  return periods;
 };
 
-const makeFakeRestaurant = (id) => {
-  const stringifiedId = id.toString();
+const convertHoursToStandard = (militaryHour) => {
+  let hour = parseInt(militaryHour, 10);
+  if (hour > 12) {
+    hour -= 12;
+  }
+  return hour.toString();
+};
+
+const generateWeekdayTextData = (periods) => {
+  const weekdayText = [];
+  const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  for (let i = 0; i < 7; i += 1) {
+    const openTime = periods[i].open.time.slice(0, 2);
+    const closeTime = periods[i].close.time.slice(0, 2);
+    const convertedOpenTime = convertHoursToStandard(openTime);
+    const convertedCloseTime = convertHoursToStandard(closeTime);
+    weekdayText.push(`${weekdays[i]}: ${convertedOpenTime}:00 AM - ${convertedCloseTime}:00 PM`);
+  }
+
+  return weekdayText;
+};
+
+const generateRestaurant = (id) => {
+  id = id.toString();
   const url = faker.internet.url();
-  const fakeHoursData = makeFakeHoursData();
-  const fakeRestaurant = {
-    place_id: stringifiedId,
-    formatted_address: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()} ${faker.address.zipCode()}, ${faker.address.country()}`,
-    international_phone_number: faker.phone.phoneNumber(),
-    url,
-    periods: fakeHoursData.periods,
-    weekday_text: fakeHoursData.weekdayText,
-    lat: faker.address.latitude(),
-    lng: faker.address.longitude(),
+  const periods = generateHoursData();
+  const weekdayText = generateWeekdayTextData(periods);
+  const restaurant = {
+    result: {
+      place_id: id,
+      name: faker.company.companyName(),
+      formatted_address: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()} ${faker.address.zipCode()}, ${faker.address.country()}`,
+      international_phone_number: faker.phone.phoneNumber(),
+      website: url,
+      url,
+      opening_hours: {
+        open_now: faker.random.boolean(),
+        periods,
+        weekday_text: weekdayText,
+      },
+      geometry: {
+        location: {
+          lat: faker.address.latitude(),
+          lng: faker.address.longitude(),
+        },
+      },
+    },
   };
-  return fakeRestaurant;
+  return restaurant;
 };
 
-module.exports = makeFakeRestaurant;
+module.exports = generateRestaurant;
--- mongodb/data/nestedSchema/seedNestedDatabase.js
+++ mongodb/data/nestedSchema/seedNestedDatabase.js
@@ -3,12 +3,12 @@ const { MongoClient } = require('mongodb');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
-const generateRestaurant = require('./generateFlatData.js');
+const generateRestaurant = require('./generateNestedData.js');
 
 const databaseHost = process.env.DATABASE_HOST || 'localhost:27017';
 const batchSize = parseInt(process.env.BATCH_SIZE, 10) || 10000;
-const numRecords = parseInt(process.env.NUM_RECORDS, 10) || 100000;
-const database = process.env.DATABASE || 'business_info_mongo_flat';
+const numRecords = parseInt(process.env.NUM_RECORDS, 10) || 10000000;
+const database = process.env.DATABASE || 'business_info_mongo_nested';
 const collectionName = process.env.COLLECTION_NAME || 'restaurants';
 
 const connectToDb = async () => {
@@ -19,8 +19,9 @@ const connectToDb = async () => {
 };
 
 const indexDb = async (collection) => {
-  await collection.createIndex({ place_id: 1 }, { unique: true });
+  await collection.createIndex({ 'result.place_id': 1 }, { unique: true });
 };
+
 
 const seedBatch = (minId, maxId, collection) => {
   return new Promise(async (resolve, reject) => {
@@ -104,3 +105,90 @@ if (cluster.isMaster) {
   console.log(`Worker ${process.pid} starting for indices ${startId}-${endId - 1}`);
   seedDb(startId, endId);
 }
+
+
+
+
+
+// const seedBatch = (minId, maxId, restaurants) => {
+//   return new Promise(async (resolve, reject) => {
+//     const restaurants = [];
+//     for (let i = minId; i < maxId; i += 1) {
+//       restaurants.push(generateRestaurant(i));
+//     }
+//     try {
+//       const savedRestaurants = await restaurants.insertMany(restaurants);
+//       resolve(savedRestaurants);
+//     } catch (error) {
+//       console.error(error);
+//       reject(error);
+//     }
+//   });
+// };
+
+// const logSeedTime = (startTime, startId, endId) => {
+//   const seedTime = (new Date().getTime() - startTime) / 1000;
+//   console.log(`Worker ${process.pid} done in ${seedTime} sec: ids ${startId}-${endId -1}`);
+// };
+
+// const seedDb = async (startId, endId) => {
+//   const startTime = new Date().getTime();
+
+//   try {
+//     const { client, restaurants } = await connectToDb();
+
+//     // seed in batches up to endId
+//     for (let i = startId; i < endId; i += batchSize) {
+//       const batchStart = i;
+//       const batchEnd = Math.min(i + batchSize, endId);
+//       await seedBatch(batchStart, batchEnd, restaurants);
+//     }
+
+//     // print out the runtime and close everything
+//     logSeedTime(startTime, startId, endId);
+//     client.close();
+//     process.exit();
+//   } catch (error) {
+//     console.error(error);
+//     process.exit();
+//   }
+// };
+
+// const createWorkers = () => {
+//   // fork workers
+//   const restaurantsPerWorker = Math.ceil(numRecords / numCPUs);
+//   for (let i = 0; i < numCPUs; i++) {
+//     const startId = i * restaurantsPerWorker;
+//     const endId = Math.min(startId + restaurantsPerWorker, numRecords);
+//     cluster.fork({ START_ID: startId, END_ID: endId });
+//   }
+
+//   cluster.on('exit', (worker) => {
+//     console.log(`worker ${worker.process.pid} exited`);
+//   });
+// };
+
+// // create workers and seed db with each worker
+
+// if (cluster.isMaster) {
+//   console.log(`Master ${process.pid} is running`);
+//   createWorkers();
+  
+//   process.on('beforeExit', async () => {
+//     try {
+//       console.log(`Master ${process.pid} indexing`);
+//       const { client, restaurants } = await connectToDb();
+//       await indexDb(restaurants);
+//       console.log('Done indexing');
+//       client.close();
+//       process.exit();
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   });
+// } else {
+//   const startId = parseInt(process.env.START_ID, 10);
+//   const endId = parseInt(process.env.END_ID, 10);
+//   console.log(`Worker ${process.pid} starting for indices ${startId}-${endId - 1}`);
+//   seedDb(startId, endId);
+// }

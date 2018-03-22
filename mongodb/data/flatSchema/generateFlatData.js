fefe1b90904ae759780042fd4364ef1b21dcab7e
David Seidenberg <davidseidenberg@gmail.com>
2018-03-22 10:51:28 -0700

    Refactor flat mongo schema data generation file

--- mongodb/data/flatSchema/generateFlatData.js
+++ mongodb/data/flatSchema/generateFlatData.js
@@ -42,37 +42,17 @@ const makeFakeRestaurant = (id) => {
   const stringifiedId = id.toString();
   const url = faker.internet.url();
   const fakeHoursData = makeFakeHoursData();
-
-  const makeFakeRestaurant = (restaurantId) => {
-    const fakeRestaurant = {
-      place_id: restaurantId,
-      formatted_address: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()} ${faker.address.zipCode()}, ${faker.address.country()}`,
-      international_phone_number: faker.phone.phoneNumber(),
-      url: faker.internet.url(),
-      weekday_text: fakeHoursData.weekdayText,
-      lat: faker.address.latitude(),
-      lng: faker.address.longitude(),
-      monday_open: fakeHoursData.periods[0].open.time,
-      monday_close: fakeHoursData.periods[0].close.time,
-      tuesday_open: fakeHoursData.periods[1].open.time,
-      tuesday_close: fakeHoursData.periods[1].close.time,
-      wednesday_open: fakeHoursData.periods[2].open.time,
-      wednesday_close: fakeHoursData.periods[2].close.time,
-      thursday_open: fakeHoursData.periods[3].open.time,
-      thursday_close: fakeHoursData.periods[3].close.time,
-      friday_open: fakeHoursData.periods[4].open.time,
-      friday_close: fakeHoursData.periods[4].close.time,
-      saturday_open: fakeHoursData.periods[5].open.time,
-      saturday_close: fakeHoursData.periods[5].close.time,
-      sunday_open: fakeHoursData.periods[6].open.time,
-      sunday_close: fakeHoursData.periods[6].close.time,
-    };
-    return fakeRestaurant;
+  const fakeRestaurant = {
+    place_id: stringifiedId,
+    formatted_address: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()} ${faker.address.zipCode()}, ${faker.address.country()}`,
+    international_phone_number: faker.phone.phoneNumber(),
+    url,
+    periods: fakeHoursData.periods,
+    weekday_text: fakeHoursData.weekdayText,
+    lat: faker.address.latitude(),
+    lng: faker.address.longitude(),
   };
-
-  const fakeRestaurantRow = makeFakeRestaurant(stringifiedId);
-
-  return { fakeRestaurantRow };
+  return fakeRestaurant;
 };
 
 module.exports = makeFakeRestaurant;

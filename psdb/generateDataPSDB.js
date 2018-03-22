2f5560671f99c8c2b092205c94a612982cd38b08
David Seidenberg <davidseidenberg@gmail.com>
2018-03-21 19:41:16 -0700

    Refactored hours data generation to break it out into two functions so they each do one thing

--- psdb/generateDataPSDB.js
+++ psdb/generateDataPSDB.js
@@ -2,13 +2,10 @@ const faker = require('faker');
 
 // *** Fake Hours Data Generator to make restaurant hours more realistic *** //
 const makeFakeHoursData = () => {
-  const fakeHoursData = {
-    periods: [],
-    weekdayText: [],
-  };
+  const periods = [];
+
   const openTimes = ['06', '07', '08', '09', '10', '11'];
   const closeTimes = ['18', '19', '20', '21', '22', '23'];
-  const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
   for (let i = 0; i < 7; i += 1) {
     const closeTime = closeTimes[Math.floor(Math.random() * 5)];
@@ -23,24 +20,39 @@ const makeFakeHoursData = () => {
         time: `${openTime}00`,
       },
     };
-    fakeHoursData.periods.push(fakeDailyHours);
-
-    const convertHoursToStandard = (militaryHour) => {
-      let hour = parseInt(militaryHour, 10);
-      if (hour > 12) {
-        hour -= 12;
-      }
-      return hour.toString();
-    };
-    fakeHoursData.weekdayText.push(`${weekdays[i]}: ${convertHoursToStandard(openTime)}:00 AM - ${convertHoursToStandard(closeTime)}:00 PM`);
+    periods.push(fakeDailyHours);
   }
 
-  return fakeHoursData;
+  return periods;
+};
+
+const convertHoursToStandard = (militaryHour) => {
+  let hour = parseInt(militaryHour, 10);
+  if (hour > 12) {
+    hour -= 12;
+  }
+  return hour.toString();
+};
+
+const makeFakeWeekdayTextData = (periods) => {
+  const weekdayText = [];
+  const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  for (let i = 0; i < 7; i += 1) {
+    const openTime = periods[i].open.time.slice(0, 2);
+    const closeTime = periods[i].close.time.slice(0, 2);
+    const convertedOpenTime = convertHoursToStandard(openTime);
+    const convertedCloseTime = convertHoursToStandard(closeTime);
+    weekdayText.push(`${weekdays[i]}: ${convertedOpenTime}:00 AM - ${convertedCloseTime}:00 PM`);
+  }
+
+  return weekdayText;
 };
 
 const makeFakeData = (id) => {
   const stringifiedId = id.toString();
   const fakeHoursData = makeFakeHoursData();
+  const fakeWeekdayTextData = makeFakeWeekdayTextData(fakeHoursData);
 
   const makeFakeRestaurant = (restaurantId) => {
     const fakeRestaurant = {
@@ -48,7 +60,7 @@ const makeFakeData = (id) => {
       formatted_address: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()} ${faker.address.zipCode()}, ${faker.address.country()}`,
       international_phone_number: faker.phone.phoneNumber(),
       url: faker.internet.url(),
-      weekday_text: fakeHoursData.weekdayText,
+      weekday_text: fakeWeekdayTextData,
       lat: faker.address.latitude(),
       lng: faker.address.longitude(),
     };
@@ -61,8 +73,8 @@ const makeFakeData = (id) => {
       const fakeHour = {
         restaurant_id: restaurantId,
         weekday: day,
-        open_time: fakeHoursData.periods[day].open.time,
-        close_time: fakeHoursData.periods[day].close.time
+        open_time: fakeHoursData[day].open.time,
+        close_time: fakeHoursData[day].close.time,
       };
       return fakeHour;
     };

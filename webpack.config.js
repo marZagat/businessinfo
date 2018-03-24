94b05419bc68b5ecfbad9b82644f3a5eb77bc703
David Seidenberg <davidseidenberg@gmail.com>
2018-03-24 13:40:12 -0700

    Updated webpack for ssr

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,57 @@
-module.exports = {
-  entry: "./client/src/index.jsx",
-  output: {
-    filename: "./client/dist/bundle.js"
-  },
+const webpack = require('webpack');
+const path = require('path');
+
+const common = {
+  context: __dirname + '/client',
   module: {
     loaders: [
-      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
-      {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/},
-      {test: /\.css$/, loader: ['style-loader', 'css-loader']}
-    ]
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'env']
+        },
+      },
+    ],
   },
-  devtool: "source-map"
-}
+};
+
+const client = {
+  entry: './client.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'app.js',
+  },
+};
+
+const server = {
+  entry: './server.js',
+  target: 'node',
+  output: {
+    path: __dirname + '/public',
+    filename: 'app-server.js',
+    libraryTarget: 'commonjs-module',
+  },
+};
+
+module.exports = [
+  Object.assign({}, common, client),
+  Object.assign({}, common, server),
+];
+
+// *** OLD WEBPACK ***
+// module.exports = {
+//   entry: "./client/src/index.jsx",
+//   output: {
+//     filename: "./client/dist/bundle.js"
+//   },
+//   module: {
+//     loaders: [
+//       {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+//       {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/},
+//       {test: /\.css$/, loader: ['style-loader', 'css-loader']}
+//     ]
+//   },
+//   devtool: "source-map"
+// }

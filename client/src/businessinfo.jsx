784cdd4fe1a8fce4304d7983d3acbd03947a46e6
David Seidenberg <davidseidenberg@gmail.com>
2018-03-26 11:51:36 -0700

    Saving version that mostly works but fails to fetch data prior to render, going to experiment with removing fetch from component

--- client/src/businessinfo.jsx
+++ client/src/businessinfo.jsx
@@ -12,6 +12,7 @@ class BusinessInfo extends React.Component {
     this.state = {
       restaurant: this.props.restaurant,
     };
+<<<<<<< 9f78012d13e49e34c2d4f126366e1d8a24878654
 <<<<<<< 8643cf5158339c5d071d0c53f79c1f06edeff0c7
     this.getRestaurantData(props.id);
   }
@@ -32,12 +33,20 @@ class BusinessInfo extends React.Component {
 =======
     getRestaurantData(props.id);
 >>>>>>> Still working on ssr, commit to compare with previous implementation
+=======
+
+    this.getRestaurantData(this.props.id);
+>>>>>>> Saving version that mostly works but fails to fetch data prior to render, going to experiment with removing fetch from component
   }
+
+  // componentDidMount() {
+  //   this.getRestaurantData(this.props.id);
+  // }
 
   getRestaurantData(id) {
     axios.get(`http://localhost:3003/api/restaurants/${id}/businessinfo`)
       .then((response) => {
-        console.log('received:', response);
+        console.log('received:', response.data);
         this.setState({ restaurant: response.data });
       }).catch((err) => {
         console.error('Failed to fetch restaurant data from server:', err);

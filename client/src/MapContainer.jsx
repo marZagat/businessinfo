72cb003a76bdac8431d2ac7533d50cda572a015f
David Seidenberg <davidseidenberg@gmail.com>
2018-04-19 15:38:59 -0700

    Adding new map icon and inputting test id temporarily (50)

--- client/src/MapContainer.jsx
+++ client/src/MapContainer.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 const mapKey = process.env.MAP_KEY;
 
-const Marker = ({ icon }) => <div><img src="http://res.cloudinary.com/madlicorice/image/upload/v1520470825/map_icon_small.png"/></div>;
+const Marker = ({ icon }) => <div><img src="./mapicon.png"/></div>;
 
 class SimpleMap extends Component {
   constructor(props) {
